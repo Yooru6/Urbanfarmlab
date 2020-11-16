72a000fd6239b123d642384fa42f3fe59026fe6c
Joonas Lehikoinen <Yooru6@gmail.com>
2020-11-16 23:22:39 +0200

    Add files via upload

---
+++ MikrokosmosAPI/README.md
@@ -0,0 +1,42 @@
+# Mikrokosmos-FirebaseCRUDAPI
+With this API you can get data from hydroponic greenhouse located in urbanfarmlab.
+
+#### JSON data example:
+{"id":"YETNqaxhk4QUv2elcq0a","name":"mikrokosmos","data":{"tvoc":266,"ph":7.98,"light":1,"co2":1624,"h":79.3,"ec":0.023,"id":1,"temp":25.5,"time":"2020-11-15 17:30:16","tempW":20.88},"publishTime":"2020-11-15T15:30:02.141Z"}
+
+### Data parameters:
+
+| Parameter | Info |
+| ----------- | ----------- |
+| id | ID |
+| name | name of the greenhouse |
+| data | Temperature(C), Humidity(%), Air Temperature(C), lightvalue(on/off), pH(1-14), EC(analog value), Co2(ppm), TVOC(ppm), Local Time(yyyy-mm-dd hh-mm-ss-) |
+| publishTime | date and time in time zone zero(yyyy-mm-dd hh:mm:ss) |
+
+### Commands:
+<ul>
+ 
+<li>Get by ID</li>
+<ul>
+   <li>https://us-central1-amiable-hydra-279814.cloudfunctions.net/mikrokosmos/api/read/IDHERE</li>
+</ul>
+ 
+<li>Get ALL</li>
+<ul>
+  <li>https://us-central1-amiable-hydra-279814.cloudfunctions.net/mikrokosmos/api/read</li>
+  <li>Ordered New->Old</li>
+</ul>
+ 
+<li>Get by DATE</li>
+<ul>
+  <li>https://us-central1-amiable-hydra-279814.cloudfunctions.net/mikrokosmos/api/read/datetime/yyyy-mm-dd</li>
+  <li>Uses time variable from data</li>
+</ul>
+ 
+ <li>Get by TIME</li>
+ <ul>
+   <li>https://us-central1-amiable-hydra-279814.cloudfunctions.net/mikrokosmos/api/read/datetime/hh:</li>
+   <li>Uses time variable from data</li>
+ </ul>
+
+</ul>
---
+++ MikrokosmosAPI/functions/index.js
@@ -0,0 +1,203 @@
+/*
+*FIREBASE CRUD API
+*Basic commands: Create, Read, Update, Delete
+*Special: Search by date
+
+1. Make database
+2. go to project settings in firebase site and add "APP"(web app)
+3. In project settings go to Service accounts and generate private key -> save private key to projects functions folder with name permissions(otherwise you cant make calls to firebase) 
+4. firebase init --> npm i express --> npm i cors
+5. Start local webserver by writing command on console(remember cd functions): npm run serve
+6. develope
+
+*/
+
+//Parameters
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+var serviceAccount = require("./permissions.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://amiable-hydra-279814.firebaseio.com"
+});
+
+const express = require('express');
+const mikrokosmos = express();
+const db = admin.firestore();
+
+const cors = require('cors');
+mikrokosmos.use(cors({origin:true}));
+
+
+//Test
+mikrokosmos.get('/hello-world', (req,res)=>{
+    return res.status(200).send('Hello World!');
+})
+
+
+//Create new
+//Post
+mikrokosmos.post('/api/create',(req,res)=>{
+    (async()=>{
+        try{
+            await db.collection('Mikrokosmos-testi').doc('/'+req.body.id + '/').create({
+
+                name: req.body.name,
+                data: req.body.data,
+                publishTime: req.body.publishTime
+
+            })
+            return res.status(200).send();
+        }catch(error){
+            console.log(error);
+            return res.status(500).send(error);
+        }
+    })();
+});
+
+//Read a specific item from database with ID
+//Get
+mikrokosmos.get('/api/read/:id',(req,res)=>{
+    (async()=>{
+        try{
+            const document = db.collection('Mikrokosmos-6-11-2020').doc(req.params.id);
+            let sensorReading = await document.get();
+            let response =sensorReading.data();
+
+            return res.status(200).send(response);
+        }catch(error){
+            console.log(error);
+            return res.status(500).send(error);
+        }
+    })();
+});
+
+//Read all items
+//Get
+mikrokosmos.get('/api/read',(req,res)=>{
+    (async()=>{
+        try{
+            let query = db.collection('Mikrokosmos-6-11-2020');
+            let response=[];          
+
+            //Get all data from firebase with query
+            await query.get().then(querySnapshot=>{
+                let docs = querySnapshot.docs;
+                
+                for (let doc of docs)
+                {
+                    const selectedItem={
+                        id: doc.id,
+                        name:doc.data().name,
+                        data: doc.data().data,
+                        publishTime: doc.data().publishTime
+                    };
+                    response.push(selectedItem);
+                }
+                return response;
+            })
+            //Make sorted list(New>Old)
+            const sortedResponse = response.sort((a, b) => new Date(b.data.time) - new Date(a.data.time)); //Sort list              
+            return res.status(200).send(sortedResponse);
+
+        }catch(error){
+            console.log(error);
+            return res.status(500).send(error);
+        }
+    })();
+});
+
+//Get by date or time
+//Get
+mikrokosmos.get('/api/read/datetime/:day',(req,res)=>{
+    (async()=>{
+        try{
+            let query = db.collection('Mikrokosmos-6-11-2020');
+            let response=[];
+            let compareList=[];
+            console.log("TRY");
+            await query.get().then(querySnapshot=>{
+                let docs = querySnapshot.docs;
+                console.log("AWAIT");
+                
+                for (let doc of docs)
+                {
+                    const selectedItem={
+                        id: doc.id,
+                        name:doc.data().name,
+                        data: doc.data().data,
+                        publishTime: doc.data().publishTime
+                    };
+
+                    //make deep copy from selectedItem and filter seconds from the time
+                    var copySelectedItem=JSON.parse(JSON.stringify(selectedItem));
+                    var responseSplit=copySelectedItem.data.time.split(':');
+                    copySelectedItem.data.time=responseSplit.slice(0,-1).join(':');
+
+                    compareList.push(copySelectedItem);
+                    response.push(selectedItem);
+                }
+                return response;
+            })
+
+            let dayList=[];
+            
+            //If value time parameter has included string add it to dayList. Using compareList variable to avoid problems when user searches with hh:mm.
+            for(var i in compareList) { 
+                if(compareList[i].data.time.includes(req.params.day)){
+                    dayList.push(response[i]);
+                }
+            }
+            //Sort daylist(new>old)
+            dayList = dayList.sort((a, b) => new Date(b.data.time) - new Date(a.data.time));
+            
+            
+            return res.status(200).send(dayList);
+
+        }catch(error){
+            console.log(error);
+            return res.status(500).send(error);
+        }
+    })();
+});
+
+
+// update
+mikrokosmos.put('/api/update/:id', (req, res) => {
+    (async () => {
+        try {
+            const document = db.collection('Mikrokosmos-testi').doc(req.params.id);
+            await document.update({
+                //item: req.body.item
+               //write fields that you want to update here!!!
+            });
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
+
+
+// delete (coming soon)
+
+/** 
+mikrokosmos.delete('/api/delete/:id', (req, res) => {
+    (async () => {
+        try {
+            const document = db.collection('Kasvihuone-Sensori-Data3').doc(req.params.id);
+            await document.delete();
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
+*/
+
+
+exports.mikrokosmos= functions.https.onRequest(mikrokosmos);
+
